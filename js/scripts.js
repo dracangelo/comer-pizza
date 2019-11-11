36631557385b4f99b557d5f2cdb179cc50e7979b
dracangelo <kelvinmbuguaw@gmail.com>
2019-11-11 18:04:17 +0300

    added a js file

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,10 @@ function validateForm() {
 }
  
 function Pizza (flavor,size,crust,toppings){
-    this.flavor = flavor,
-    this.size = size,
-    this.crust = crust,
-    this.toppings = toppings
+    this.flavor = flavor;
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
 };
 
 Pizza.prototype.order = function() {
@@ -24,16 +24,40 @@ Pizza.prototype.order = function() {
 }
 
 function Cost (price,quantity,delivery) {
-    this.price = price,
-    this.quantity = quantity,
-    this.delivery = delivery,
+    this.price = price;
+    this.quantity = quantity;
+    this.delivery = delivery;
 };
 
 Cost.prototype.totalCost = function () {
-    return this.price + this.quantity + this.delivery
+    return this.price * this.quantity + this.delivery
 };
 
-var sizeCost = [1050, 750, 500];
+var sizeCost = [1050, 750, 500,];
 var transport = [0, 250];
+$ (document).ready(function(){
+    $('form#mine').submit(function (event) {
+        event.preventDefault()
+var newflavour=$('flavour').val();
+var newSize = $('size').val();
+var newCrust = $('crust').val();
+var newToppings =$('toppings').val();
+var delivery=$('delivery').val();
+var newQuantity=$('quantity').val();
+var newPrice = sizeCost[newSize-1];
+var newDelivery = transport[delivery-1]
+var newOrder = new order(newflavour,newSize,newCrust,newToppings);
+var newTotal = new totalCost(newPrice,newQuantity,newDelivery);
+if(delivery===1) {
+    alert(newOrder.order());
+    alert(newTotal.totalCost());
+}
+else if (delivery===2) { 
+    prompt("please enter destination");
+    alert("Delivery fee is 250 ")
+    alert(newOrder.order());
+    alert(newTotal.totalCost());
 
-document.getElementById("")
+}
+});
+});
